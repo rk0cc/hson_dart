19c4eddb273e6741fc02ec4c50b3a4ce728aaf84
Cyrus Chan <cyruschan1212@gmail.com>
2021-10-24 15:29:04 +0800

    Push with tests

---
+++ test/hson_test.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+
+import 'package:hson_dart/hson.dart';
+import 'package:path/path.dart' as p;
+import 'package:test/test.dart';
+
+void main() {
+  String samplePath = p.join("hson", "sample.hson");
+  HSON hson = HSON.getInstance();
+  late File targetFile;
+  late Directory hsonF;
+  setUpAll(() async {
+    hsonF = Directory("hson");
+    if (!(await hsonF.exists())) {
+      hsonF = await hsonF.create();
+    }
+  });
+  test("Write test", () async {
+    hson.writeHSON<Map<String, String>>({"foo": "bar"}, samplePath);
+    targetFile = File(samplePath);
+    expect(true, await targetFile.exists());
+  });
+  test("Read test", () {
+    var context = hson.readHSON(samplePath);
+    expect(context, equals({"foo": "bar"}));
+  });
+}
